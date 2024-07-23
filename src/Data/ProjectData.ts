342f615e3ae7a8696deed5b5e5f8c67cbb2f8ff0
Antonio <antonio.ibarra99@outlook.es>
2024-07-22 17:47:25 -0700

    Projects Added

---
+++ src/Data/ProjectData.ts
@@ -0,0 +1,122 @@
+type projectDataProps = {
+  image: string;
+  Tecnologies: string;
+  Name: string;
+  Description: string;
+  Url: string;
+  Advertencia?: string;
+};
+
+export const projectData: projectDataProps[] = [
+  {
+    image: '1',
+    Tecnologies: 'HTML + CSS',
+    Name: 'Arquitectura Bosque',
+    Description:
+      'Construcción de casas con una variedad de modelos dependiendo de las necesidades del cliente',
+    Url: 'https://arquitectura-bosque-page.vercel.app/',
+  },
+  {
+    image: '2',
+    Tecnologies: 'HTML + CSS',
+    Name: 'TechPro',
+    Description:
+      'Venta de audífonos con diferentes modelos disponibles dependiendo de las necesidades de los consumidores',
+    Url: 'https://tech-pro-page.vercel.app/',
+  },
+  {
+    image: '3',
+    Tecnologies: 'HTML + CSS',
+    Name: 'Delivery Food',
+    Description:
+      'Búsqueda y venta de alimentos en base a los gustos de cada cliente',
+    Url: 'https://delivery-page.vercel.app/',
+  },
+  {
+    image: '4',
+    Tecnologies: 'HTML + CSS',
+    Name: 'Podcast FM',
+    Description:
+      'Publicación de podcast con diferentes planes que se adaptan a las necesidades de cada usuario',
+    Url: 'https://podcast-page-two.vercel.app/',
+  },
+  {
+    image: '5',
+    Tecnologies: 'HTML + CSS',
+    Name: 'Airbnb',
+    Description: 'Búsqueda de viajes a diferentes lugares turísticos del mundo',
+    Url: 'https://air-travel-page.vercel.app/',
+  },
+  {
+    image: '6',
+    Tecnologies: 'HTML + CSS',
+    Name: 'Cafetería',
+    Description:
+      'Promoción de cafetería incluyendo el menú disponible y los horarios de atención',
+    Url: 'https://cafeteria-page-two.vercel.app/',
+  },
+  {
+    image: '7',
+    Tecnologies: 'React JS + Tailwind Css',
+    Name: 'Administrador de clientes',
+    Description:
+      'Administrador de registros y modificaciones de clientes en el sistema',
+    Url: 'https://crm-clients-red.vercel.app/',
+    Advertencia:
+      'Esta aplicación podría no mostrar la información registrada debido a la consulta local de la información ',
+  },
+  {
+    image: '8',
+    Tecnologies: 'React TS + Tailwind Css + Axios',
+    Name: 'Buscador de bebidas',
+    Description:
+      'Buscador de bebidas dependiendo de algún ingrediente o categoría',
+    Url: 'https://drinks-ts.vercel.app/',
+    Advertencia:
+      'Esta aplicación podría no funcionar correctamente debido a la consulta externa de una API ',
+  },
+  {
+    image: '9',
+    Tecnologies: 'React JS + Tailwind Css + Axios',
+    Name: 'Pronostico del clima',
+    Description: 'Consulta del clima en tiempo real dependiendo de cada ciudad',
+    Url: 'https://weather-app-lake-alpha.vercel.app/',
+    Advertencia:
+      'Esta aplicación podría no funcionar correctamente debido a la consulta externa de una API ',
+  },
+  {
+    image: '10',
+    Tecnologies: 'React TS + Tailwind Css',
+    Name: 'Calculadora de propinas',
+    Description: 'Calcula las propinas de una lista de alimentos consumidos',
+    Url: 'https://weather-app-lake-alpha.vercel.app/',
+  },
+  {
+    image: '11',
+    Tecnologies: 'React TS + Tailwind Css + LocalStorage',
+    Name: 'Contador de calorías',
+    Description: 'Genera el balance de calorías consumidas y quemadas',
+    Url: 'https://calories-tracker-wheat.vercel.app/',
+  },
+  {
+    image: '12',
+    Tecnologies: 'React TS + Tailwind Css + Zustand ',
+    Name: 'Administrador de veterinaria',
+    Description: 'Seguimiento de pacientes de una veterinaria',
+    Url: 'https://patient-administrator-tau.vercel.app/',
+  },
+  {
+    image: '13',
+    Tecnologies: 'React TS + Tailwind Css + Zustand + Axios ',
+    Name: 'Cotizador de criptomonedas',
+    Description: 'Cotización de criptomonedas en tiempo real',
+    Url: 'https://cripto-viewer.vercel.app/',
+  },
+  {
+    image: '14',
+    Tecnologies: 'React TS + Css ',
+    Name: 'Tienda de guitarras',
+    Description: 'Venta de guitarras',
+    Url: 'https://guitar-store-sigma.vercel.app/',
+  },
+];
--- src/Views/Projects.tsx
+++ src/Views/Projects.tsx
@@ -1,5 +1,19 @@
 import Card from '../Component/Card';
+import { projectData } from '../Data/ProjectData';
 
 export default function Projects() {
-  return <Card />;
+  return (
+    <div className="flex flex-wrap gap-4 items-center justify-center m-8">
+      {projectData.map((data) => (
+        <Card
+          key={data.Name}
+          image={data.image}
+          Tecnologies={data.Tecnologies}
+          Name={data.Name}
+          Description={data.Description}
+          Url={data.Url}
+        />
+      ))}
+    </div>
+  );
 }
